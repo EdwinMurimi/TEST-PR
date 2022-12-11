6ead536a10d417d59d63c275b80158593b1f5267
Edwin Murimi <edwinfmaina19@gmail.com>
2022-12-11 21:26:52 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+
+
+const app = express();
+
+app.get('/v1/sample-data', (req, res) => {
+    res.status(200).send([
+        {
+            id: 1,
+            data: "lorem ipsum S ajd skd dkln dkl"
+        },
+        {
+            id: 2,
+            data: "lorem ipsum S ajd skd dkln dkl"
+        },
+        {
+            id: 3,
+            data: "lorem ipsum S ajd skd dkln dkl"
+        },
+        {
+            id: 4,
+            data: "lorem ipsum S ajd skd dkln dkl"
+        },
+        {
+            id: 5,
+            data: "lorem ipsum S ajd skd dkln dkl"
+        },
+        {
+            id: 6,
+            data: "lorem ipsum S ajd skd dkln dkl"
+        }
+    ])
+})
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`application running on this address => http://localhost:${PORT}`);
+})
